4dd637315bb9147961e4685b9cef832b2526022b
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2019-09-14 20:46:33 -0400

    Added download features and integration with revAI module

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ WIRE PROTOCOL --
 POST:
     {"path" : string pointing to filepath of the audio} -> True or False, depending on whether the audio is of a
     person under the influence.
-
+    {"link" : link to the Google Cloud Object Storage object. The object must be public.
 """
 import os
+import requests
 from dataclasses import dataclass
 
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 
+from revAI import RevAI
+
+AUDIO_FILE_EXTENSION = ".mp3"
+BUCKET_NAME = "lgtm_hackmit/o/"
+REV_AI = RevAI()
 app = Flask(__name__)
 api = Api(app)
 
-a = ".mp3"
 
 
 @dataclass
@@ -28,11 +33,33 @@ class Analysis:
     drunk: bool
 
 
+def download(url: str) -> str:
+    """
+    Download file to temporary storage
+    :param url: Path to the Google Cloud Object Storage file. Must be a public object.
+    :return: file path
+    """
+    # Extracting the filename from the url
+    # Based on the assumption that the filename is between the bucket name and '?'
+    start_index = url.index(BUCKET_NAME) + len(BUCKET_NAME)
+    filename = url[start_index: url.index("?")]
+    r = requests.get(url)
+    with open(filename, "wb") as code:
+        code.write(r.content)
+    return filename
+
+
 def analyze(filename: str) -> Analysis:
     # TODO: Feed the audio file into the api interface.
     if not os.path.isfile(filename):
         raise ValueError("Path does not exist.")
+    print("made it.")
+    rev_ai_output = REV_AI.get_transcript(filename)
+    print("finished.")
+    # Find some way to compare the example text with the output.
+    print("Transcript: " + rev_ai_output)
     analysis = Analysis(drunk=True)
+
     return analysis
 
 
@@ -41,9 +68,13 @@ class LGTM(Resource):
     def post(self) -> tuple:
         parser = reqparse.RequestParser()
         parser.add_argument("path")
+        parser.add_argument("link")
         args = parser.parse_args()
-
         audio_file_path = args["path"]
+        google_object_link = args["link"]
+
+        if google_object_link is not None:
+            audio_file_path = download(google_object_link)
         try:
             analysis = analyze(audio_file_path)
         except ValueError:
--- revAI.py
+++ revAI.py
@@ -1,10 +1,11 @@
 from rev_ai import apiclient
 import json
 
-f=open("revAI_accessToken.txt", 'r')
-ACCESS_TOKEN = f.read()  
+f = open("revAI_accessToken.txt", 'r')
+ACCESS_TOKEN = f.read()
 
-class RevAI: 
+
+class RevAI:
 
     def __init__(self):
         # create your client
@@ -12,23 +13,22 @@ class RevAI:
 
     def get_transcript(self, path):
         # Use when want a new transcript from rev.ai
-        """job = self.client.submit_job_local_file(path)
+        job = self.client.submit_job_local_file(path)
         job_details = self.client.get_job_details(job.id)
-        while(self.client.get_job_details(job.id).status.name != "TRANSCRIBED"):
+        while self.client.get_job_details(job.id).status.name != "TRANSCRIBED":
+            print(self.client.get_job_details(job.id).status.name)
             pass
         # Get only the first speaker in case of background noise
-        transcript_json = self.client.get_transcript_json(job.id)["monologues"][0]"""
+        transcript_json = self.client.get_transcript_json(job.id)["monologues"][0]
 
         # Uncomment when trying to conserve time on rev.ai
-        f = open("transcript.json")
+        # f = open("transcript.json")
         # Get only the first speaker in case of background noise
-        transcript_json = json.load(f)["monologues"][0]                                         
+        transcript_json = json.load(f)["monologues"][0]
 
         transcript_text = ""
         for i in transcript_json["elements"]:
             transcript_text += (i["value"])
-
-        print(transcript_text)
 
         return transcript_text
 
